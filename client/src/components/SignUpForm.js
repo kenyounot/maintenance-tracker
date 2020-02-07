b049250da1885e368a14ee3485e7751238685274
Kenneth Spatta <kenyounot@gmail.com>
2020-02-06 21:49:41 -0500

    Convert sign up form to a class, and implement class state to handle form inputs

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
 
 export class SignUpForm extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			name: '',
+			email: '',
+			password: '',
+			passwordConfirmation: ''
+		};
+	}
+
+	handleOnChange = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+
 	render() {
+		console.log(this.state);
+
 		return (
 			<div>
 				<h2>Sign Up Below</h2>
-				<form>
-					<input type='text' placeholder='Name' />
+				<form className='signup-form'>
+					<input
+						onChange={this.handleOnChange}
+						type='text'
+						name='name'
+						placeholder='Name'
+						value={this.state.name}
+					/>
 					<br />
-					<input type='email' placholder='Email' />
+					<input
+						onChange={this.handleOnChange}
+						type='email'
+						name='email'
+						placholder='Email'
+						value={this.state.email}
+					/>
 					<br />
-					<input type='password' placeholder='Password' />
+					<input
+						onChange={this.handleOnChange}
+						type='password'
+						name='password'
+						placeholder='Password'
+						value={this.state.password}
+					/>
 					<br />
-					<input type='password' placeholder='Password Confirm' />
+					<input
+						onChange={this.handleOnChange}
+						type='password'
+						name='passwordConfirmation'
+						placeholder='Password Confirm'
+						value={this.state.passwordConfirmation}
+					/>
 					<br />
 					<button type='submit'>Submit</button>
 				</form>
