fc852ef8a9e664f0f43f6aa6140d68541bb7360c
Kenneth Spatta <kenyounot@gmail.com>
2020-02-08 18:53:02 -0500

    Create Welcome component for the garage landing page

---
+++ client/src/components/GarageWelcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GarageWelcome = props => {
+	return (
+		<div>
+			<h2>{props.userName}'s Garage</h2>
+		</div>
+	);
+};
+
+export default GarageWelcome;
--- client/src/containers/Garage.js
+++ client/src/containers/Garage.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import GarageWelcome from '../components/GarageWelcome';
 
 export class Garage extends Component {
 	constructor(props) {
 		super(props);
 	}
 
+	renderCars = vehicles => {
+		return vehicles.map(vehicle => {});
+	};
+
 	render() {
-		return <div>Garage</div>;
+		return (
+			<div>
+				<GarageWelcome userName={this.props.userName} />
+			</div>
+		);
 	}
 }
 
-export default connect()(Garage);
+const mapStateToProps = state => {
+	return {
+		userName: state.userReducer.currentUser.name
+	};
+};
+
+export default connect(mapStateToProps)(Garage);
