2579fd04cd63adde874c2df41fd251190e99f197
Kenneth Spatta <kenyounot@gmail.com>
2020-02-08 20:42:10 -0500

    Created conditional to render message if no vehicles are present within the state

--- client/src/containers/Garage.js
+++ client/src/containers/Garage.js
@@ -4,12 +4,20 @@ import GarageWelcome from '../components/GarageWelcome';
 import Vehicles from '../components/Vehicles';
 
 export class Garage extends Component {
+	renderVehiclesOrMessage = () => {
+		if (this.props.vehicles.length < 1) {
+			return <h2>No vehicles in your garage.</h2>;
+		} else {
+			return <Vehicles vehicles={this.props.vehicles} />;
+		}
+	};
+
 	render() {
 		return (
 			<div>
 				<GarageWelcome userName={this.props.userName} />
 				<button>Add Vehicle</button>
-				<Vehicles vehicles={this.props.vehicles} />
+				{this.renderVehiclesOrMessage()}
 			</div>
 		);
 	}
