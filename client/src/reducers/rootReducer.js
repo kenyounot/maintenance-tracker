18b7cadde675f1205f6c71b4d614dbb0273c6ac6
Kenneth Spatta <kenyounot@gmail.com>
2020-02-11 03:14:23 -0500

    Create maintenaceReducer and import it within rootReducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import vehicleReducer from './vehicleReducer';
+import maintenanceReducer from './maintenanceReducer';
 
-export default combineReducers({ userReducer, vehicleReducer });
+export default combineReducers({ userReducer, vehicleReducer, maintenanceReducer });
