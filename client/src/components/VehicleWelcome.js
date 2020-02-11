df3b78741c88c4a3cc425e9d988b948d05fe91ce
Kenneth Spatta <kenyounot@gmail.com>
2020-02-10 22:32:52 -0500

    Add Maintenance model that belongs to a Vehicle

--- client/src/components/VehicleWelcome.js
+++ client/src/components/VehicleWelcome.js
@@ -3,7 +3,9 @@ import React from 'react';
 const VehicleWelcome = props => {
 	return (
 		<div>
-			Here's Your {props.make} {props.model}'s maintenance history.
+			<h3>
+				Here's Your {props.make} {props.model}'s maintenance history.
+			</h3>
 		</div>
 	);
 };
---
+++ db/migrate/20200211033127_create_maintenances.rb
@@ -0,0 +1,14 @@
+class CreateMaintenances < ActiveRecord::Migration[6.0]
+  def change
+    create_table :maintenances do |t|
+      t.string :type
+      t.integer :mileage
+      t.string :parts
+      t.text :description
+      t.integer :price
+      t.belongs_to :vehicle, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
